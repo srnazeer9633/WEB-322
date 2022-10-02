8c21deea472d65e0308f349ca36174d3128f791a
srnazeer9633 <srnazeer@myseneca.ca>
2022-10-01 21:17:17 -0700

    first

---
+++ A1/server.js
@@ -0,0 +1,11 @@
+const exp=require("express");
+const app=exp();
+const path= require("path");
+app.get("/",function(req,res)
+{
+    console.log(__dirname);
+    res.send("Subin Raj Nazeer 123044216");
+    //res.sendFile(path.join(__dirname,"home.html"));
+});
+var port=process.env.PORT || 3000;
+app.listen(port);
